2355a1fd957cc27ba9d1c0c04a86ac82fe31a3a4
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2023-01-20 21:40:20 +0000

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,108 +12,25 @@
 
 #include "get_next_line.h"
 
-int	theres_no_nl(char *line)
-{
-	int	i;
-
-	i = 0;
-	while (line[i])
-	{
-		if (line[i] == '\n')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	ft_strlen(const char *s)
-{
-	int	counter;
-
-	counter = 0;
-	while (s[counter])
-		counter++;
-
-	return (counter);
-}
-
-char	*save_leftovers(char *buffer, int pos)
-{
-	char	*leftovers;
-	int		i;
-
-	i = 0;
-	if (ft_strlen(buffer) > pos)
-		leftovers = malloc(sizeof(char) * (ft_strlen(buffer) - pos));
-	while (buffer[pos])
-		leftovers[i++] = buffer[pos++];
-	return (leftovers);
-}
-
-char	*ft_strjoin(char *prefix, char *suffix)
-{
-	char		*new_str;
-	int			i;
-	int			j;
-	char		*stash;
-
-	if (stash == NULL)
-		printf("stash n exist\n");
-	else
-		stash = ft_strjoin(stash, prefix);
-
-	printf("stash : %s\n", stash);
-	i = 0;
-	j = 0;
-	new_str = (char *)malloc(ft_strlen(prefix) + ft_strlen(suffix) + 1);
-	if (!new_str)
-		return (NULL);
-	while (prefix[i])
-	{
-		new_str[i] = prefix[i];
-		i++;
-	}
-	free(prefix);
-	while (suffix[j])
-	{
-		new_str[i++] = suffix[j++];
-		if (suffix[j - 1] == '\n')
-			break ;
-	}
-	stash = save_leftovers(suffix, i);
-	return (new_str);
-}
-
-
-char	*copy_line()
-{
-	
-}
-
 char	*get_next_line(int fd)
 {
-	char			*buffer;
-	char			*line;
-	int				chars_read;
-	static char		*stash;
+	static char	buff[BUFFER_SIZE + 1];
+	char		*line;
+	int			i;
 
-	read(fd, buffer, BUFFER_SIZE);
-	while (theres_no_nl(buffer))
+	i = 0;
+	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
 	{
-		read(fd, buffer, BUFFER_SIZE);
-		line = ft_strjoin(line, buffer);
+		while (buff[i])
+			buff[i++] = 0;
+		return (NULL);
+	}
+	line = NULL;
+	while (buff[0] || read(fd, buff, BUFFER_SIZE) > 0)
+	{
+		line = ft_strjoin(line, buff);
+		if (ft_limpamemo(buff))
+			break ;
 	}
 	return (line);
-}
-
-int	main(void)
-{
-	int		fd;
-	fd = open("poema.txt", O_RDONLY);
-	//printf("%s", get_next_line(fd));
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
-	return (0);
 }
